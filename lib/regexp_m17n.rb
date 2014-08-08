1eef93962a60c4e6971359e0ab6f11163b8fd5dd
Paul Callaghan <paul.callaghan@transcendit.co.uk>
2014-08-08 10:57:30 +0100

    I've solved it.
    (a) There's two encodings for which I don't have converters, or for which
        none exist. These are now being skipped in the test code. If you don't skip
        them, then it's an error in the test code and the lib code is never called.
    
    (b) Sometimes, using regexps is overkill. This is one of the important things
        to learn on route to mastery of regexps! (I used to teach this stuff (-:)
    
        Instead, my solution does a far simpler test: it's true if the string is
        not nil? and if it is different from the "" value in the relevant encoding.
    
        This bypasses any trip into regexp library, and it doesn't require a
        full string scan (compare << (str = '%099999999d' % 0) =~ /./ >> vs
        << (str = '%099999999d' % 0) =~ /.+/ >>).

--- lib/regexp_m17n.rb
+++ lib/regexp_m17n.rb
@@ -1,5 +1,6 @@
 module RegexpM17N
   def self.non_empty?(str)
-    str =~ /^.+$/
+    !str.nil? && str != "".encode(str.encoding)   
+    # OR # !str.nil? && !str.chars.empty? 
   end
 end
--- test/regexp_m17n_test.rb
+++ test/regexp_m17n_test.rb
@@ -5,6 +5,8 @@ require_relative '../lib/regexp_m17n'
 class RegexpTest < MiniTest::Unit::TestCase
   def test_non_empty_string
     Encoding.list.each do |enc|
+      next if enc.name == "ISO-2022-JP-2"   # no converter exists
+      next if enc.name == "UTF-7"           # no converter exists
       assert(RegexpM17N.non_empty?('.'.encode(enc)))
     end
   end
